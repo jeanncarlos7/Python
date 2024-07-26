8a5f54869b62ae60bdbca633f8a04a0ddbb336b6
Jean Carlos <127451299+jeanncarlos7@users.noreply.github.com>
2024-07-26 12:49:06 -0300

    Add files via upload

---
+++ converte_minusculo.py
@@ -0,0 +1,13 @@
+def main():
+    lista_palavras = ["FIAP","TDS","Cazuza","ManGa"]
+
+    lista_minusculas = list(map(converte_minusculo,lista_palavras))
+
+    print(lista_minusculas)
+
+
+def converte_minusculo (palavra):
+    return(palavra.lower())
+
+if (__name__ == "__main__"):
+    main()
