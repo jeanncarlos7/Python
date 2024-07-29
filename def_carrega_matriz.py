d6f295ab74230268e8359b6a4a4967d3f0923056
Jean Carlos <127451299+jeanncarlos7@users.noreply.github.com>
2024-07-29 11:07:51 -0300

    Add files via upload

---
+++ def_carrega_matriz.py
@@ -0,0 +1,34 @@
+def main():
+    matriz = []
+    nlin = int(input("Digite o numero de linhas da matriz: "))
+    ncol = int(input("Digite o numero de colunas da matriz: "))
+    carrega_matriz(matriz, nlin, ncol)
+    print(f"A média dos elementos da matriz é {calcula_media_matriz (matriz,nlin,ncol):.2f}")
+    exibe_impares(matriz, nlin, ncol)
+
+
+
+def carrega_matriz (matriz,nlin,ncol):
+    for lin in range (nlin):
+        linha = []
+        for col in range (ncol):
+            linha.append(int(input("Digite o elemento da matriz: ")))
+        matriz.append(linha)
+
+def calcula_media_matriz (matriz,nlin,ncol):
+    soma = 0
+    for lin in range (nlin):
+        for col in range (ncol):
+            soma+=matriz[lin][col]
+
+    media = soma / (nlin * ncol)
+    return (media)
+
+def exibe_impares (matriz,nlin,ncol):
+    for lin in range (nlin):
+        for col in range (ncol):
+            if (matriz[lin][col] % 2 != 0):
+                print(matriz[lin][col])
+
+if (__name__ == "__main__"):
+    main()
