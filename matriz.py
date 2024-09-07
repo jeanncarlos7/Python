475603e848050c6969978124a06ecddc022c6181
Jean Carlos <127451299+jeanncarlos7@users.noreply.github.com>
2024-09-07 09:56:48 -0300

    Add files via upload

---
+++ matriz.py
@@ -0,0 +1,27 @@
+'''
+Faça um programa que leia uma matriz 3x3 de inteiros e multiplique
+os elementos da diagonal principal da matriz por um número k.
+Imprima a matriz na tela antes e depois da multiplicação .
+'''
+
+matriz = []
+
+for lin in range(3):
+    linha = []
+    for col in range(3):
+        linha.append(int(input("Digite um elemento da matriz: ")))
+    matriz.append(linha)
+
+for lin in range(3):
+    print(matriz[lin])
+
+k = int(input("Digite o valor de k: "))
+
+for lin in range(3):
+    for col in range(3):
+        if (lin == col): #condicao para o elemento da matriz estar na diagonal principal
+            matriz[lin][col] = matriz[lin][col] * k  #matriz[lin][col]*=k
+
+for lin in range(3):
+    print(matriz[lin])
+
