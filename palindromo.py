d0920c8a51e99d60e44d016da1b8b6f5823e0e29
Jean Carlos <127451299+jeanncarlos7@users.noreply.github.com>
2024-09-15 11:04:30 -0300

    Add files via upload

---
+++ palindromo.py
@@ -0,0 +1,7 @@
+str_original = input("Digite a string: ")
+str_invertida = str_original[::-1]
+
+if (str_original == str_invertida):
+    print("A palavra eh um palindromo")
+else:
+    print("A palavra nao eh um palindromo")
